1e5ad7b5895ceb469634d28c487b74e1f69548d2
CoderCyrus <cyrusbilibili@gmail.com>
2020-02-13 17:03:08 +0100

    tp_symfony

---
+++ PHP-SYMFPNY/test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+        <?php $i = 2; print "Result is ".$i;?><br/>
+        <h5>Test PHP $_GET method</h5>
+        <?php echo 'Context entered in URL is :  ' . htmlspecialchars($_GET["name"]);?><br/>
+        <?php  if (($_GET["name"]) == 'admin')
+         // http://127.0.0.1/prweb/tp2/test.php/?name=admin 
+         {
+            echo 'OK.';
+        }else {
+            echo 'WRONG.';
+        }
+        ?>
+        <h5>Test PHP $_POST method</h5>
+        
+    </body>
+</html>
